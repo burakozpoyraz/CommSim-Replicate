496af80923972af9ca27da436e16b11c226da311
Burak Özpoyraz <burakozpoyraz@gmail.com>
2023-01-26 23:08:59 +0100

    Create Algorithm.m

---
+++ Index Modulation/1) Spatial Modulation - Optimal Detection and Performance Analysis/Algorithm.m
@@ -0,0 +1,21 @@
+%% Paper Information
+% Title: Spatial Modulation: Optimal Detection and Performance Analysis
+%
+% Authors:
+% 1) Jeyadeepan Jeganathan
+% 2) Ali Ghrayeb
+% 3) Leszek Szczecinski
+%
+% Journal: IEEE Communications Letters
+%
+% Date: August 2008
+%
+% Abstract: In this letter, we derive the optimal detector for the
+% so-called spatial modulation (SM) system introduced by Mesleh et al. in
+% [1]. The new detector performs significantly better than the original 
+% (~4 dB gain), and we support our results by deriving a closed form
+% expression for the average bit error probability. Performance gains
+% (~1.5 - 3 dB) over popular multiple antenna systems, making it an
+% excellent candidate for future wireless communication standards.
+
+%% Figure-2
